6655ad17edd9dacd1700a8f2757077e601ff124e
Grenish rai <mrcoder2033d@gmail.com>
2025-12-04 13:09:27 +0530

    Add Umami analytics script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const space = Space_Grotesk({
@@ -21,7 +22,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${space.className} antialiased`}>{children}</body>
+      <body className={`${space.className} antialiased`}>
+        {children}
+        <Script
+          src="https://cloud.umami.is/script.js"
+          data-website-id="c8e55daa-a555-4f8a-9a22-baef339a92e9"
+          strategy="afterInteractive"
+        />
+      </body>
     </html>
   );
 }
